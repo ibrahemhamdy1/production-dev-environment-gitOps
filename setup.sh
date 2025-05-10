ec0c2d7ab7741eca26e3e3693fba6fa5dc4dcf5a
Ibrahim hamdy <ibrahim@techflipp.com>
2025-05-10 11:43:30 +0300

    wip.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
-# #!/usr/bin/env bash
-# set -euo pipefail
-# THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+#!/usr/bin/env bash
+set -euo pipefail
+THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# # Install helm if missing
-# if ! command -v helm &>/dev/null; then
-#   echo "Installing Helm..."
-#   TMP=$(mktemp -d)
-#   curl -sSL https://get.helm.sh/helm-$(curl -sSL https://api.github.com/repos/helm/helm/releases/latest | grep -Po '"tag_name":\s*"v\K[0-9.]+')-$(uname | tr '[:upper:]' '[:lower:]')-amd64.tar.gz -o $TMP/helm.tgz
-#   tar -C $TMP -xzf $TMP/helm.tgz
-#   sudo mv $TMP/*/helm /usr/local/bin/
-# fi
+# Install helm if missing
+if ! command -v helm &>/dev/null; then
+  echo "Installing Helm..."
+  TMP=$(mktemp -d)
+  curl -sSL https://get.helm.sh/helm-$(curl -sSL https://api.github.com/repos/helm/helm/releases/latest | grep -Po '"tag_name":\s*"v\K[0-9.]+')-$(uname | tr '[:upper:]' '[:lower:]')-amd64.tar.gz -o $TMP/helm.tgz
+  tar -C $TMP -xzf $TMP/helm.tgz
+  sudo mv $TMP/*/helm /usr/local/bin/
+fi
 
 # microk8s kubeconfig
 if [[ "$(kubectl config current-context 2>/dev/null)" == "microk8s" ]]; then
